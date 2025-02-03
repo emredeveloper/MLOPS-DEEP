ec189192485de7252708e46bb9f9239e88a50bb9
emre <emredeveloperr@gmail.com>
2025-02-03 03:30:08 +0300

    update init.py

--- deepseek_mlops/__init__.py
+++ deepseek_mlops/__init__.py
@@ -3,6 +3,5 @@ from .preprocessing import preprocess
 from .train import train_model
 from .evaluate import evaluate_model
 from .save_model import save_trained_model
-```
 
 # Empty __init__.py to mark this directory as a package.
